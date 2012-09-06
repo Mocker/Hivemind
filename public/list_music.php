1eea7563ed053dd0cfee67580c11cc7e9eb07df5
Mocker <ryan@ryanguthrie.com>
2012-09-06 21:31:36 +0000

    list music blah

--- public/list_music.php
+++ public/list_music.php
@@ -7,6 +7,20 @@
 	output as json for easy ajaxing
 **** ***** */
 header("Access-Control-Allow-Origin: *");
+
+// Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+        exit(0);
+    }
+
+
 header('Content-type: application/json');
 $playlist = "recommends";
 
